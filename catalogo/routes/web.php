ebadc7b093fa7216986815840d0dd0189aaf7507
Lautaro Rodriguez <compros.adm02@capsantafe.org.ar>
2021-01-20 10:32:23 -0300

    peticion y vista adminMarcas

--- catalogo/routes/web.php
+++ catalogo/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MarcaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// use App\Http\Controllers\MarcaController;
+//No funcion el use App
+
+####### CRUD de marcas
+Route::get('/adminMarcas', [ MarcaController::class, 'index' ] );
